9c5355512b783b2dff0a2bb08d3e159bdad5a4bb
guddansingh <guddansingh9391@gmail.com>
2024-06-15 14:16:24 +0530

    add new file

---
+++ walrus_operator.py
@@ -0,0 +1,9 @@
+#this is an walrus operator that used to create loops 
+
+#data = input("enter some data : ")
+#while data:
+    #print(data)
+    #data = input("enter some data : ")
+
+while (data := input("enter some data : ")):
+    print(data) #this expression do the same work as above
