9459b796d1ca9bba5308a40ed11950cb6dbd3577
guddansingh <guddansingh9391@gmail.com>
2024-06-15 21:56:06 +0530

    add  new file

--- walrus_operator.py
+++ walrus_operator.py
@@ -5,5 +5,24 @@
     #print(data)
     #data = input("enter some data : ")
 
-while (data := input("enter some data : ")):
-    print(data) #this expression do the same work as above
+#while (data := input("enter some data : ")):
+    #print(data) #this expression do the same work as above4
+
+#this is an another example of the walrus expression
+sample_data = [
+	{"userId": 1, "name": "rahul", "completed": False},
+	{"userId": 1, "name": "rohit", "completed": False},
+	{"userId": 1, "name": "ram", "completed": False},
+	{"userId": 1, "name": "ravan", "completed": True}
+]
+
+print("With Python 3.8 Walrus Operator:") 
+for entry in sample_data: 
+	if name := entry.get("name"):
+		print(f'Found name: "{name}"')
+
+print("Without Walrus operator:")
+for entry in sample_data:
+	name = entry.get("name")
+	if name:
+		print(f'Found name: "{name}"')
